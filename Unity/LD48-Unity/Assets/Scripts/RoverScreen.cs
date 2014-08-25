24ee8e0b993fe193a1de6300d1b4ea7ef76912f4
Eralp Karaduman <eralpkaraduman@gmail.com>
2014-08-25 14:47:28 +0300

    screen print init

--- Unity/LD48-Unity/Assets/Scripts/RoverScreen.cs
+++ Unity/LD48-Unity/Assets/Scripts/RoverScreen.cs
@@ -4,10 +4,50 @@ using System.Collections;
 public class RoverScreen : MonoBehaviour {
 
 	private GUITexture background;
+	public GUIText screenGUIText;
+
+	public string question = "";
+
+	private float maxCharsPerLine = 31;
 	//private bool powerOn;
 	// Use this for initialization
 	void Start () {
+
 		background = GetComponent<GUITexture> ();
+
+		question = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nec mauris a sagittis. Nam lobortis ornare semper. Mauris feugiat tortor sed turpis dictum, ultrices venenatis nibh tempus.";
+		printScreen ();
+
+	}
+
+	void printScreen(){
+
+		// print question
+		string text = "EARTH: "+question;
+
+		Debug.Log (text);
+
+		string linedText = "";
+		int numChars = text.Length;
+		int charIndex = 0;
+		int charsInLine = 0;
+		while (numChars>0) {
+
+			if(charsInLine>=maxCharsPerLine){
+				linedText += "\n";
+				charsInLine = 0;
+			}
+			linedText += text[charIndex];
+
+			charIndex++;
+			charsInLine++;
+			numChars--;
+		}
+		screenGUIText.text = linedText;
+
+		// print print seperator
+		screenGUIText.text += "\n-------------------------------\n";
+
 	}
 	
 	// Update is called once per frame
